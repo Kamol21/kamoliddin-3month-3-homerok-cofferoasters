3ac2b86ae26ced5b42b0338038411c71a6dd2d56
Kamol21 <kamolakangdev@gmail.com>
2022-08-03 12:50:02 +0500

    fix some problems

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+var elModalTogleButton = document.querySelector(".site-header__btn");
 var elModalOpenButton = document.querySelector(".plan-s3__buttom-box-button");
+var elModalNavbar = document.querySelector(".site-header");
 var elModal = document.querySelector(".modal");
+
+elModalTogleButton.addEventListener("click", function(){
+  elModalNavbar.classList.toggle("site-header__nav-show");
+})
 
 elModalOpenButton.addEventListener("click", function(){
   elModal.classList.add("modal-show");
@@ -11,3 +17,8 @@ var elModalCloseButton = document.querySelector(".modal-close-button");
 elModalCloseButton.addEventListener("click" , function (){
   elModal.classList.remove("modal-show");
 })
+
+
+
+
+
